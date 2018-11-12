a9de00b2f61ddd207748dcdd71cb2a07a4d0bb74
yt t <tyt1992@gmail.com>
2018-11-12 14:20:25 -0800

    V0.01
    same as phantom

--- phantom/avsignal.m
+++ phantom/avsignal.m
@@ -4,47 +4,10 @@ close all;
 clear all;
 clc;
 tic
-% load data from .csv files
-% files = dir('**/*.csv');
-% count = zeros(1);
-% PATH = {'start'};
-% data = {};
-% j = 0;
-% for i = 1:1:length(files)
-%     NEWPATH = files(i).folder;
-%     NAME = files(i).name;
-%     A = xlsread([NEWPATH '/' NAME]);
-%     if strcmp(NEWPATH , PATH{end})
-%         count(j) = count(j)+1;
-%         data{j}{count(j)} = A;        
-%     else
-%         j = j+1;
-%         PATH{j} = NEWPATH;
-%         count(j) = 1;
-%         data{j}{count(j)} = A; 
-%     end
-% end
-% toc
-% %% calibrate so that every sequence start from 0 time
-% cali_data = {};
-% for i = 1:1:length(data)
-%     for j = 1:1:length(data{i})
-%         [rows,~] = find(data{i}{j}(:,1)<0);
-%         cali_data{i}(j,:) = [data{i}{j}(length(rows)+1:end,2);zeros(length(rows),1)];
-%     end
-% end
-% 
-% %% average of each sample group(3 folders)
-% num_f = 3;
-% avg = [];
-% for i = 1:num_f:length(count)
-%     tempsum = zeros(1,size(cali_data{i},2));
-%     for j = 0:1:num_f-1
-%         tempsum = tempsum + mean(cali_data{i+j});
-%     end
-%     avg(floor(i/num_f)+1,:) = tempsum./num_f;
-% end
-% save('avdata.mat','avg');
+
+
+%% some parameters
+num_f = 3;      %num of files per measurement
 
 %% load data from .mat
 files = dir('**/*.mat'); 
@@ -100,26 +63,63 @@ for i = 1:1:M
     for j = 1:1:count(i)
         point0 = ceil(abs(startTime(i,j))/intevalTime(i,j));
         %point0 = point0 - 1; %when use pico 6000, mat file has one less data point
-        cali_data{i}(j,:) = [data{i}(j,point0+1:end),zeros(1,point0)];
+         tempdata = [data{i}(j,point0+1:end),zeros(1,point0)];
+         cali_data{i}(j,:) = tempdata(1:25000);               %set all signal to same length
     end
 end
 
-%% average of each sample group(3 folders)
-num_f = 3;
+%% average of each sample group(num_f folders)
 avg = [];
 for i = 1:num_f:length(count)
     tempsum = zeros(1,size(cali_data{i},2));
     for j = 0:1:num_f-1
-        tempsum = tempsum + mean(cali_data{i+j});
+        tempsum = tempsum + median(cali_data{i+j});
     end
-    avg(floor(i/num_f)+1,:) = tempsum./num_f;
+    avg(floor((i-1)/num_f)+1,:) = tempsum./num_f;
 end
 save('avdata.mat','avg');
 toc
-%% preprocessing of data
-% for i = 1:1:length(data)
-%     infdata = isinf(data{i});
-%     data{i}(infdata) = 0;
+
+%% load data from .csv files
+% files = dir('**/*.csv');
+% count = zeros(1);
+% PATH = {'start'};
+% data = {};
+% j = 0;
+% for i = 1:1:length(files)
+%     NEWPATH = files(i).folder;
+%     NAME = files(i).name;
+%     A = xlsread([NEWPATH '/' NAME]);
+%     if strcmp(NEWPATH , PATH{end})
+%         count(j) = count(j)+1;
+%         data{j}{count(j)} = A;        
+%     else
+%         j = j+1;
+%         PATH{j} = NEWPATH;
+%         count(j) = 1;
+%         data{j}{count(j)} = A; 
+%     end
 % end
+% toc
+% %% calibrate so that every sequence start from 0 time
+% cali_data = {};
+% for i = 1:1:length(data)
+%     for j = 1:1:length(data{i})
+%         [rows,~] = find(data{i}{j}(:,1)<0);
+%         cali_data{i}(j,:) = [data{i}{j}(length(rows)+1:end,2);zeros(length(rows),1)];
+%     end
+% end
+% 
+% %% average of each sample group(3 folders)
+% num_f = 3;
+% avg = [];
+% for i = 1:num_f:length(count)
+%     tempsum = zeros(1,size(cali_data{i},2));
+%     for j = 0:1:num_f-1
+%         tempsum = tempsum + mean(cali_data{i+j});
+%     end
+%     avg(floor(i/num_f)+1,:) = tempsum./num_f;
+% end
+% save('avdata.mat','avg');
 
 
